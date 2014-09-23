ddb4e7b84facd6ce3ded8938a1727dac06c4bfab
Gan Sze Kai <szekai@gmail.com>
2014-09-23 13:46:59 +0800

    add izpack

--- script/bin/run.sh
+++ script/bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+APP_PATH=$INSTALL_PATH
 if [ -z $APP_PATH ]; then
   APP_PATH=.
 fi
@@ -7,7 +7,7 @@ fi
 
 CLASSPATH=.:$APP_PATH/lib/*
 #LOG_CONF=$APP_PATH/conf/logback.xml
-LOG_CONF=$APP_PATH/conf/log4j.xml
+LOG_CONF=file:$APP_PATH/conf/log4j.xml
 #JAVA_OPTS="-classpath $CLASSPATH -Dlogback.configurationFile=$LOG_CONF -DAPP_PATH=$APP_PATH"
 JAVA_OPTS="-classpath $CLASSPATH -Dlog4j.configuration=$LOG_CONF -DAPP_PATH=$APP_PATH"
 java $JAVA_OPTS sky.logger.MyApp1
